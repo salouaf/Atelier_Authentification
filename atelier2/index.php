6f068404e661231c82be8d12601ec1daa2303e5c
salouaf <s.florand@cfacampusmontsouris.fr>
2025-11-05 11:51:48 +0100

    index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Vérification simple du username et de son password.
     // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
     if ($username === 'admin' && $password === 'secret') {
-        setcookie('authToken', '12345', time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) / j'ai modieir à 60 pour 1min 
+        setcookie('authToken', $token, time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) / j'ai modieir à 60 pour 1min 
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
     } else {
